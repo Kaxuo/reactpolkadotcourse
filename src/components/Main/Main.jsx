5ef542dc6761b3f02db239aa06cc0f4d7327fb98
Kevin Nguyen <kevinnguyen@Kevins-MacBook-Pro.local>
2021-09-06 09:24:08 +0200

    transaction now working

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,12 +3,29 @@ import { SubstrateContext } from './../../substrate-lib/BlockChainContext';
 import './Main.scss';
 
 function Main() {
-  const { accounts, balance, connection, loadAccounts } =
-    useContext(SubstrateContext);
+  const {
+    accounts,
+    balance,
+    connection,
+    loadAccounts,
+    transfer,
+    status,
+    setform,
+    form,
+  } = useContext(SubstrateContext);
   useEffect(async () => {
     await connection();
-    await loadAccounts();
+    let unsub = await loadAccounts();
+    return () => unsub && unsub();
   }, []);
+
+  const ellipsis = (string) =>
+    string.length > 5 &&
+    `${string.substring(0, 8)} ... ${string.substr(string.length - 10)}`;
+
+  const onChange = (e) => {
+    setform({ ...form, [e.target.name]: e.target.value });
+  };
 
   const displayAccounts = () => {
     return (
@@ -17,13 +34,32 @@ function Main() {
           return (
             <div className='accounts' key={account.address}>
               <span className='name'>{account.meta.name}</span>
-              <span className='address'>{account.address}</span>
+              <span className='address small'>{ellipsis(account.address)}</span>
+              <span className='address big'>{account.address}</span>
               <span className='balance'>
                 {balance && balance[account.address]}
               </span>
             </div>
           );
         })}
+        <div className='form'>
+          <div className='input'>
+            <span>To</span>
+            <input type='text' name='to' value={form.to} onChange={onChange} />
+          </div>
+          <div className='input'>
+            <span>amount</span>
+            <input
+              type='text'
+              name='amount'
+              value={form.amount}
+              onChange={onChange}
+            />
+          </div>
+          <button onClick={transfer}>Transfer</button>
+        </div>
+
+        <h1>{status}</h1>
       </div>
     );
   };
--- src/substrate-lib/BlockChainContext.js
+++ src/substrate-lib/BlockChainContext.js
@@ -1,12 +1,17 @@
 import { useState, createContext } from 'react';
 import { ApiPromise, WsProvider } from '@polkadot/api';
-import { web3Accounts, web3Enable } from '@polkadot/extension-dapp';
+import {
+  web3Accounts,
+  web3Enable,
+  web3FromSource,
+} from '@polkadot/extension-dapp';
 import keyring from '@polkadot/ui-keyring';
 
 export const SubstrateContext = createContext();
 
 export const SubstrateProvider = ({ children }) => {
   let api;
+
   // Make the connection with the blockchain //
   const connection = async () => {
     const wsProvider = new WsProvider('ws://127.0.0.1:9944');
@@ -19,6 +24,15 @@ export const SubstrateProvider = ({ children }) => {
       let accs = await web3Accounts();
       keyring.loadAll({ isDevelopment: true }, accs);
       let allAccounts = keyring.getPairs();
+      allAccounts = allAccounts.map(({ address, meta }) => {
+        return {
+          address: address,
+          meta: {
+            ...meta,
+            name: `${meta.name} ${meta.isInjected ? `(${meta.source})` : ''}`,
+          },
+        };
+      });
       const addresses = allAccounts.map((account) => account.address);
       let unsubscribeAll = null;
       api.query.system.account
@@ -37,17 +51,70 @@ export const SubstrateProvider = ({ children }) => {
         })
         .catch(console.error);
       setaccounts(allAccounts);
-      return () => unsubscribeAll && unsubscribeAll();
+      return unsubscribeAll;
     } catch (e) {
       console.error(e);
     }
   };
 
+  // Get Selected account , for the moment, the account is hardcoded //
+  const getFromAcct = async () => {
+    const wsProvider = new WsProvider('ws://127.0.0.1:9944');
+    api = await ApiPromise.create({ provider: wsProvider });
+    const acc = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
+    const tempAcct = keyring.getPair(acc);
+    const {
+      address,
+      meta: { source, isInjected },
+    } = tempAcct;
+    let fromAcct;
+    if (isInjected) {
+      const injected = await web3FromSource(source);
+      fromAcct = address;
+      api.setSigner(injected.signer);
+    } else {
+      fromAcct = tempAcct;
+    }
+    return fromAcct;
+  };
+
+  // Methods to transfer token to another account //
+  const transfer = async () => {
+    const data = [form.to, form.amount];
+    const fromAcct = await getFromAcct();
+    const txExecute = api.tx.balances.transfer(...data);
+    const unsub = await txExecute
+      .signAndSend(fromAcct, txResHandler)
+      .catch(txErrHandler);
+    setUnsub(() => unsub);
+  };
+
+  // Status text //
+  const txResHandler = ({ status }) =>
+    status.isFinalized
+      ? setStatus(`😉 Finalized. Block hash: ${status.asFinalized.toString()}`)
+      : setStatus(`Current transaction status: ${status.type}`);
+  const txErrHandler = (err) =>
+    setStatus(`😞 Transaction Failed: ${err.toString()}`);
+
   const [accounts, setaccounts] = useState([]);
   const [balance, setbalance] = useState({});
+  const [status, setStatus] = useState('Nothing for the moment');
+  const [unsub, setUnsub] = useState(null);
+  const [form, setform] = useState({ to: '', amount: '' });
   return (
     <SubstrateContext.Provider
-      value={{ balance, accounts, connection, loadAccounts }}
+      value={{
+        balance,
+        accounts,
+        connection,
+        loadAccounts,
+        transfer,
+        status,
+        unsub,
+        setform,
+        form,
+      }}
     >
       {children}
     </SubstrateContext.Provider>
